e103634b5a5323001868b78929e2e48e3a446d74
Ted Lawless <lawlesst@gmail.com>
2014-10-06 13:06:04 -0400

    Add is_online.

--- lib/bul_utils.rb
+++ lib/bul_utils.rb
@@ -14,3 +14,22 @@ def suppressed(record)
     return true
   end
 end
+
+#Returns true if record is available online
+#
+#Identify whether a given record is available online.
+#Uses the location code found in item records.
+#Brown location codes beginning with "es" indicate the item
+#is available online.
+def is_online(record)
+  extractor = MarcExtractor.new("945l")
+  values = extractor.collect_matching_lines(record) do |field, spec, extractor|
+    extractor.collect_subfields(field, spec)
+  end.compact
+  values.each do |val|
+    if val.start_with?("es")
+      return true
+    end
+  end
+  return false
+end
--- spec/bul_utils_spec.rb
+++ spec/bul_utils_spec.rb
@@ -7,12 +7,22 @@ describe "bul_utils" do
     expect(is_sup).to be_truthy
   end
 
-  it "identifies a suppressed record" do
-    rec = read('suppressed.mrc')
-    @indexer.instance_eval do
-      to_field "hide", suppressed
-    end
-    output = @indexer.map_record(rec)
-    expect(output["hide"]).to eq [true]
+  it "identifies a non-suppressed record" do
+    rec = read('named_collection.mrc')
+    is_sup = suppressed(rec)
+    expect(is_sup).to be_falsey
   end
+
+  it "correctly identifies an online record" do
+    rec = read('ejournal.mrc')
+    is_online = is_online(rec)
+    expect(is_online).to be true
+  end
+
+  it "correctly identifies a non-online record" do
+    rec = read('book_880.mrc')
+    is_online = is_online(rec)
+    expect(is_online).to be false
+  end
+
 end
