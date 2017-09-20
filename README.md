bf18456f47eba855cb4b796c6884289f80f5f27f
Hector Correa <hector_correa@brown.edu>
2017-09-20 10:55:58 -0400

    Adds process to delete from Solr records marked as suppressed in Millennium

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # bul-traject
 This project transforms MARC records into Solr documents using the [Traject](https://github.com/traject-project/traject) tools developed by [Bill Dueber](https://github.com/billdueber/) and [Jonathan Rochkind](https://github.com/jrochkind).
 
-[![Build Status](https://secure.travis-ci.org/Brown-University-Library/bul-traject.png?branch=master)](https://travis-ci.org/Brown-University-Library/bul-traject)
-
 Run as:
 ```
 traject -c config.rb -u http://localhost:8081/solr/blacklight-core /full/path/to/marcfile.mrc
@@ -16,4 +14,30 @@ display the output to the console (and not push the data to Solr).
 ```
 traject --debug-mode -c config.rb /full/path/to/marcfile.mrc
 ```
- 
+
+
+## Handling suppressed records
+In order to delete documents from Solr for records that have been deleted in
+Millennium we use a two step process. This is a bit of a kludge but it works OK
+for now.
+
+First, run Traject with a special configuration file to output to a JSON
+file the IDs (BIB #) of any records that have been marked as suppressed.
+
+```
+bundle exect traject -w JsonWriter -c config_delete.rb /full/path/to/update_rec_N_file.mrc > to_delete.json
+```
+
+Then process this JSON file to actually delete the records from Solr.
+
+```
+source .env
+ruby process_delete.rb to_delete.json
+```
+
+(.env defines SOLR_URL)
+
+Notice that in this case we run Traject against our daily *update* MARC files,
+not against our full MARC files. The daily update files only include records
+that changed in the last day or two. (TODO: how long does it take to run it
+against the full files?)
---
+++ process_delete.rb
@@ -0,0 +1,128 @@
+# Processes a line delimited file where each line contains the JSON
+# representation of a Solr document ID to delete. The file to process
+# is produced via Traject with the `config_delete.rb` configurationn.
+# Each line on the file to process looks as follows:
+#
+# {"id":["b3187374"]}
+# {"id":["b3397561"]}
+#
+# Syntax:
+# SOLR_ENV=http://servername/solrcore ruby process_delete.rb file_name.json
+#
+require "json"
+require "net/http"
+# require "byebug"
+
+class Processor
+  def initialize(file_name, solr_url, debug_mode)
+    @file_name = file_name
+    @solr_url = solr_url
+    @debug_mode = true
+    if @debug_mode
+      @MockResponse = Struct.new(:code)
+    end
+  end
+
+  def http_get(url)
+    if @debug_mode
+      log_msg("HTTP GET #{url}")
+      return @MockResponse.new("200")
+    end
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    if url.start_with?("https://")
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+  end
+
+  def http_post(url, payload)
+    if @debug_mode
+      log_msg("HTTP POST #{url} with #{payload}")
+      return @MockResponse.new("200")
+    end
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    if url.start_with?("https://")
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.add_field("Content-Type", "text/xml")
+    request.add_field("Accept", "application/json")
+    request.body = payload
+    response = http.request(request)
+  end
+
+  def delete_from_solr(id)
+    url = "#{@solr_url}/update"
+    payload = "<delete><query>id:#{id}</query></delete>"
+    response = http_post(url, payload)
+    if response.code == "200"
+      log_msg("Deleted #{id} OK")
+    else
+      log_warn("Solr returned HTTP #{response.code} for #{id}")
+    end
+  end
+
+  def commit_solr()
+    url = "#{@solr_url}/update?commit=true"
+    response = http_get(url)
+    if response.code == "200"
+      log_msg("Committed changes to Solr OK")
+    else
+      log_warn("Solr returned HTTP #{response.code} when committing changes")
+    end
+  end
+
+  def is_valid_id?(id)
+    return false if id == nil
+    return id[0] == "b" && id.length == 8
+  end
+
+  def log_msg(msg)
+    puts "#{Time.now} INFO: #{msg}"
+  end
+
+  def log_warn(msg)
+    puts "#{Time.now} WARN: #{msg}"
+  end
+
+  def process_file()
+    count = 0
+    deleted = 0
+    log_msg("Processing: #{@file_name}, Solr: #{@solr_url}")
+
+    File.readlines(@file_name).each do |line|
+      count += 1
+      json = JSON.parse(line)
+      id = (json["id"] || []).first
+      if is_valid_id?(id)
+        delete_from_solr(id)
+        deleted += 1
+      else
+        log_warn("Skipped line #{count}, id: #{id}.")
+      end
+    end
+
+    if deleted > 0
+      commit_solr()
+    end
+
+    log_msg("Done processing #{@file_name}. Deleted #{deleted} out of #{count}.")
+  end
+end
+
+
+if ARGV.count != 1
+  abort("No JSON file with IDs to delete was received")
+end
+
+if ENV["SOLR_URL"] == nil
+  abort("No SOLR_URL was found on the environment")
+end
+
+p = Processor.new(ARGV[0], ENV["SOLR_URL"], true)
+p.process_file()
