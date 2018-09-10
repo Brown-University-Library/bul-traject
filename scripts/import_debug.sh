0bd29ae79fd6df9e26a2b71c591231889565a239
Hector Correa <hector_correa@brown.edu>
2018-09-10 12:42:52 -0400

    Current test files

--- scripts/import_debug.sh
+++ scripts/import_debug.sh
@@ -1,2 +1,2 @@
-MARC_FILE=./data/combined_1.mrc
+MARC_FILE=./data/combined_7.mrc
 bundle exec traject --debug-mode -w JsonWriter -c config.rb $MARC_FILE
--- scripts/import_one.sh
+++ scripts/import_one.sh
@@ -1,10 +1,10 @@
 SOLR_URL=http://localhost:8081/solr/blacklight-core
-MARC_FILE=./path/to/marc/file.mrc
+MARC_FILE=./data/combined_15.mrc
 
 echo "Importing"
 echo "  $MARC_FILE"
 echo "  $SOLR_URL"
-traject -c config.rb -u $SOLR_URL $MARC_FILE
+bundle exec traject -c config.rb -u $SOLR_URL $MARC_FILE
 
 curl $SOLR_URL/update?commit=true
 echo "Done."
