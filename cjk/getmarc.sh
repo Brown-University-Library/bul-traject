a1b37400af4939b838c726a5043990c50e9adf1d
Hector Correa <hector_correa@brown.edu>
2019-10-02 16:29:11 -0400

    Notes and test scripts for CJK support

---
+++ cjk/getmarc.sh
@@ -0,0 +1,13 @@
+BIB_SERVICE_URL=http://insert-url-here/bibService
+
+curl $BIB_SERVICE_URL/marc/?bib=b3254870 > b3254870.mrc
+curl $BIB_SERVICE_URL/marc/?bib=b2443451 > b2443451.mrc
+curl $BIB_SERVICE_URL/marc/?bib=b3254953 > b3254953.mrc
+curl $BIB_SERVICE_URL/marc/?bib=b7084450 > b3254953.mrc
+curl $BIB_SERVICE_URL/marc/?bib=b3432221 > b3254953.mrc
+curl $BIB_SERVICE_URL/marc/?bib=b6524958 > b3254953.mrc
+curl $BIB_SERVICE_URL/marc/?bib=b4106491 > b3254953.mrc
+curl $BIB_SERVICE_URL/marc/?bib=b7835116 > b7835116.mrc
+curl $BIB_SERVICE_URL/marc/?bib=b3666424 > b3666424.mrc
+curl $BIB_SERVICE_URL/marc/?bib=b3666425 > b3666425.mrc
+curl $BIB_SERVICE_URL/marc/?bib=b7996223 > b7996223.mrc
