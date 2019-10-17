2c844fce816f47cca32e00b580184de7ee2fb0b7
Hector Correa <hector_correa@brown.edu>
2019-10-17 11:27:13 -0400

    Better logging

--- scripts/import_debug.sh
+++ scripts/import_debug.sh
@@ -1,2 +1,3 @@
-MARC_FILE=/Users/hectorcorrea/data/b2724484.mrc
-bundle exec traject --debug-mode -w JsonWriter -c config.rb $MARC_FILE > b2724484.json
+MARC_FILE=/Users/hectorcorrea/data/combined_11.mrc
+bundle exec traject --debug-mode -w JsonWriter -c config.rb $MARC_FILE > combined_11.json 2>&1
+
