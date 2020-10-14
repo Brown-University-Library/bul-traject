75924ccbed9167e28aad47f135acdc0754e8d1a7
Hector Correa <hector_correa@brown.edu>
2020-10-14 08:41:39 -0400

    Utility script

--- scripts/import_one.sh
+++ scripts/import_one.sh
@@ -1,12 +1,10 @@
-MARC_FILE=~/data/combined/combined_05.mrc
+MARC_FILE=~/data/combined/combined_02.mrc
 SOLR_URL=http://localhost:8983/solr/josiah7
-export CJK=true
 
 echo "Importing"
 echo "  $MARC_FILE"
 echo "  $SOLR_URL"
-# >out.txt 2>&1
-bundle exec traject -c config.rb -u $SOLR_URL $MARC_FILE
+bundle exec traject -c config.rb -u $SOLR_URL $MARC_FILE >out.txt 2>&1
 
 curl $SOLR_URL/update?commit=true
 echo "Done."
